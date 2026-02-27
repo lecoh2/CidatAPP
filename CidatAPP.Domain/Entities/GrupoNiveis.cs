b89c86ce94ea09829129e3538d5d22b855bd25ab
lecoh2 <lecoh2@hotmail.com>
2026-02-27 13:11:30 -0300

    Criação das classes usuario niveis e enumerador do status

---
+++ CidatAPP.Domain/Entities/GrupoNiveis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CidatAPP.Domain.Entities
+{
+    internal class GrupoNiveis
+    {
+    }
+}
