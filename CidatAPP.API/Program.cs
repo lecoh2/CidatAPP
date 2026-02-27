afa20fea948259c334b32bb4b582e5a63aeff288
lecoh2 <lecoh2@hotmail.com>
2026-02-27 09:32:24 -0300

    Configuração das bibliotecas de documentação da API(SWAGER e SCALAR)

--- CidatAPP.API/Program.cs
+++ CidatAPP.API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+//SWAGGER
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -13,6 +18,12 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+//SWAGGER
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//SCALAR
+app.MapScalarApiReference(s => s.WithTheme(ScalarTheme.BluePlanet));
 
 app.UseAuthorization();
 
